1b028703e3de856fc05e28acba8d7470a0eb1019
AustenGameStop <AustenElam@gamestop.com>
2016-10-17 14:42:59 -0500

    changing to WPF

---
+++ IoT Dallas of Things WPF/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.IO.Ports;
+using System.Net.Http;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+
+namespace IoT_Dallas_of_Things_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            SerialPort mySerialPort = new SerialPort("COM5");
+
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            mySerialPort.Open();
+
+            InitializeComponent();
+
+            //disable text-box UI in preperation for first scan
+            RFIDTextBlock.Visibility = Visibility.Hidden;
+            FNameTextBox.Visibility = Visibility.Hidden;
+            LNameTextBox.Visibility = Visibility.Hidden;
+            PhoneTextBox.Visibility = Visibility.Hidden;
+            AddressTextBox.Visibility = Visibility.Hidden;
+            BusNumTextBox.Visibility = Visibility.Hidden;
+
+            TagLabel.Visibility = Visibility.Hidden;
+            BusLabel.Visibility = Visibility.Hidden;
+            FirstNameLabel.Visibility = Visibility.Hidden;
+            LastNameLabel.Visibility = Visibility.Hidden;
+            PhoneLabel.Visibility = Visibility.Hidden;
+            AddressLabel.Visibility = Visibility.Hidden;
+
+            CheckInButton.Visibility = Visibility.Hidden;
+            
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+
+
+            this.Dispatcher.Invoke(() =>
+            {
+                GreetingTextBlock.Text = "";
+                RFIDTextBlock.Content = indata;
+                //FNameTextBlock.Text = "First Name:";
+            });
+        }
+
+        private async void create_Click(object sender, RoutedEventArgs e)
+        {
+            //Client ID: paNHXLgIJxzQItbjHOm3VWZEqJ3soMAd
+            //Client Secret: SbFM86Kj69jFj6eR
+            var token = "Bearer 2|sRATc5iweO8FUfjd4bQQjw4ACMaL";
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.us1.covisint.com/");
+
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.com.covisint.platform.device.v2+json");
+            client.DefaultRequestHeaders.Add("Authorization", token);
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "device/v3/tasks/createDeviceFromTemplate?deviceTemplateId=2ccb21ac-dc8c-44cd-a9a7-5b5ddf87fd43");
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            responseBody = responseBody.Replace("\"", "").Replace(@"\", "");
+
+            string tagID = getBetween(responseBody, "id:", ",");
+        }
+
+
+        private async void update_Click(object sender, RoutedEventArgs e)
+        {
+            string tagID = "4c295e3f-2b97-47eb-9798-10f92058418e";
+
+            var token = "Bearer 2|sRATc5iweO8FUfjd4bQQjw4ACMaL";
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.us1.covisint.com/");
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.com.covisint.platform.device.v2+json;fetchattributetypes=true;fetcheventtemplates=true;fetchcommandtemplates=true");
+            client.DefaultRequestHeaders.Add("Authorization", token);
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/device/v3/devices/" + tagID);
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            //response = await client.PostAsync("device/" + tagID, requestContent);
+
+            //var requestContent = new FormUrlEncodedContent(new[] {
+            //new KeyValuePair<string, string>("00887147-365e-46a7-8c49-965fe4fb784c", "1"),
+            //    });
+            //HttpResponseMessage response = await client.PostAsync("/devices/" + tagID, requestContent);
+
+
+
+        }
+
+
+        private void scan_Click(object sender, RoutedEventArgs e)
+        {
+            GreetingTextBlock.Visibility = Visibility.Hidden;
+
+            RFIDTextBlock.Visibility = Visibility.Visible;
+            RFIDTextBlock.IsEnabled = false;
+            FNameTextBox.Visibility = Visibility.Visible;
+            LNameTextBox.Visibility = Visibility.Visible;
+            PhoneTextBox.Visibility = Visibility.Visible;
+            AddressTextBox.Visibility = Visibility.Visible;
+            BusNumTextBox.Visibility = Visibility.Visible;
+
+            TagLabel.Visibility = Visibility.Visible;
+            BusLabel.Visibility = Visibility.Visible;
+            FirstNameLabel.Visibility = Visibility.Visible;
+            LastNameLabel.Visibility = Visibility.Visible;
+            PhoneLabel.Visibility = Visibility.Visible;
+            AddressLabel.Visibility = Visibility.Visible;
+
+            CheckInButton.Visibility = Visibility.Visible;
+        }
+
+
+
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private void CheckIn_Click(object sender, RoutedEventArgs e)
+        {
+            GreetingTextBlock.Visibility = Visibility.Visible;
+
+            RFIDTextBlock.Visibility = Visibility.Hidden;
+            FNameTextBox.Visibility = Visibility.Hidden;
+            LNameTextBox.Visibility = Visibility.Hidden;
+            PhoneTextBox.Visibility = Visibility.Hidden;
+            AddressTextBox.Visibility = Visibility.Hidden;
+            BusNumTextBox.Visibility = Visibility.Hidden;
+
+            TagLabel.Visibility = Visibility.Hidden;
+            BusLabel.Visibility = Visibility.Hidden;
+            FirstNameLabel.Visibility = Visibility.Hidden;
+            LastNameLabel.Visibility = Visibility.Hidden;
+            PhoneLabel.Visibility = Visibility.Hidden;
+            AddressLabel.Visibility = Visibility.Hidden;
+
+            CheckInButton.Visibility = Visibility.Hidden;
+        }
+    }
+}
